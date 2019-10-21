e58afb4b60eeacd64355e92f4eac6886da7a6009
Karl Han <karl-han@iwktd.com>
2019-10-21 12:11:03 +0800

    This is a branch with changing xor number.

--- aes/temp.c
+++ aes/temp.c
@@ -1,7 +1,24 @@
 #include <assert.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
-#include "crypto.h"
+
+short multiply(short a, short b, short xor_num) {
+    short res = 0;
+    while (a && b) {
+        if (b & 1) {
+            res ^= a;
+        }
+        // Test if a will overflow when *2
+        if (a & 0x80) {
+            a = (a << 1) ^ xor_num;
+        } else {
+            a <<= 1;
+        }
+        b >>= 1;
+    }
+    return res % 256;
+}
 
 bool test_new_modulo(short xor_num) {
     bool arr[0x100];
@@ -30,7 +47,9 @@ bool test_new_modulo(short xor_num) {
 
 int main() {
     assert(test_new_modulo(0x1b) == true);
+    printf("Pass xor 0x1b\n");
     assert(test_new_modulo(0x1d) == true);
+    printf("Pass xor 0x1d\n");
 
     assert(test_new_modulo(0x1c) == false);
     return 0;
