4c9414112c2c9ba9132fdaad757b739502deae48
Karl Han <karl-han@iwktd.com>
2019-11-20 09:48:22 +0800

    Wrong iv lead to a strange result.

--- aes/aes_algor/src/main.rs
+++ aes/aes_algor/src/main.rs
@@ -2,12 +2,17 @@ mod lib;
 mod tests;
 
 use lib::{basic_operations::*, key_msg::*, op_modes::*};
+use rand::{thread_rng, RngCore};
 use std::convert::TryInto;
 use std::env;
 use std::fs::File;
 use std::io::{Read, Result, Write};
 
 fn main() {
+    let mut rng = thread_rng();
+    let mut iv = [0u8; 16];
+    rng.fill_bytes(&mut iv);
+    let iv: u128 = u128::from_le_bytes(iv);
     if let Ok(_) = env::var("ENC") {
         let toml = "Cargo.toml";
         let target = "test.cipher";
@@ -17,7 +22,7 @@ fn main() {
         let mut f = File::create(key_file).expect("Failed to create `key`");
         f.write(&k.msg()).expect("Error while writing key file");
 
-        M_row::ctr_build(k, toml, target);
+        M_row::cbc_build(k, toml, target, iv);
     } else {
         let cipher = "test.cipher";
         let plain = "test.plain";
@@ -28,6 +33,6 @@ fn main() {
         let res = f.read(&mut key).expect("Wrong while read file");
         assert_eq!(res, 16);
 
-        M_row::ctr_destruct(Key::new(&key), cipher, plain);
+        M_row::cbc_destruct(Key::new(&key), cipher, plain, iv);
     }
 }
